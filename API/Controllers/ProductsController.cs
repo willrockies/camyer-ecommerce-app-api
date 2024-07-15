2fa68af7a7fcb2e97881fea5cf118b1b368f4a96
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-07-15 15:52:02 -0300

    38.Using the specification methods in the controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,7 @@
 using Core.Entities;
 using Core.Interfaces;
-using Infrastructure.Data;
+using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -25,7 +24,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
         {
-            var products = await _productRepo.ListAllAsync();
+            var spec = new ProductsWithTypesSpecification();
+            var products = await _productRepo.ListAsync(spec);
             
             return Ok(products);
         }
---
+++ Core/Specifications/ProductsWithTypesSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            
+        }
+    }
+}
