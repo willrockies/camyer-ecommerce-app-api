795a5512692fe2d0727cf11ea7a7e77ba7283365
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-07-16 16:29:32 -0300

    47.creating a test controller for errors

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,7 +9,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productRepo;
         private readonly IGenericRepository<ProductType> _productTypeRepo;
