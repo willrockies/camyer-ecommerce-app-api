779dbad0c1300fce20ecad0338c7157559064433
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-26 14:01:40 -0300

    411. Setting up the identity classes

---
+++ Core/Entities/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Net;
+
+namespace Core.Entities.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public Address Address { get; set; }
+    }
+}
